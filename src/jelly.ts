acf409f881ac75d3cd7345eceab9b48cb3068c9e
tompng <tomoyapenguin@gmail.com>
2021-07-14 23:27:07 +0900

    jelly simulation

---
+++ src/jelly.ts
@@ -0,0 +1,169 @@
+import { Point3D } from './math'
+import type { String3D } from './string'
+type ShapeParam = {
+  size: number
+  theta1: number
+  theta2: number
+  innerDistance: number
+  innerRadius: number
+}
+type HardnessParam = {
+  middle: number
+  inner: number
+  outer: number
+  topMiddle: number
+  topInner: number
+  middleOuter: number
+  innerOuter: number
+  innerMiddle: number
+}
+
+type JellyPoint = { p: Point3D; v: Point3D; f: Point3D }
+type JellyLink = { a: JellyPoint; b: JellyPoint; r: number; k: number }
+
+function genPoint(x: number, y: number, z: number): JellyPoint {
+  return {
+    p: { x, y, z },
+    v: { x: 0, y: 0, z: 0 },
+    f: { x: 0, y: 0, z: 0 }
+  }
+}
+
+function genLink(a: JellyPoint, b: JellyPoint, k: number): JellyLink {
+  const r = Math.hypot(a.p.x - b.p.x, a.p.y - b.p.y, a.p.z - b.p.z)
+  return { a, b, r, k }
+}
+
+export class Jelly {
+  innerStrings: String3D[] = []
+  outerStrings: String3D[] = []
+  topPoint = genPoint(0, 0, 0)
+  middlePoints: JellyPoint[] = []
+  outerPoints: JellyPoint[] = []
+  innerPoints: JellyPoint[] = []
+  points: JellyPoint[] = []
+  links: JellyLink[] = []
+  shrinkRadialLinks: JellyLink[] = []
+  shrinkOutlineLinks: JellyLink[] = []
+  pointStrings: { p: JellyPoint; s: String3D }[] = []
+  constructor(public numInner: number, public numOuter: number, public shape: ShapeParam, public hardness: HardnessParam) {
+    this.setInitialPosition()
+    this.setLinks()
+  }
+  assignStrings(innerStrings: String3D[], outerStrings: String3D[]) {
+    const { pointStrings, numInner, numOuter, innerPoints, outerPoints } = this
+    this.innerStrings = innerStrings
+    this.outerStrings = outerStrings
+    this.pointStrings = []
+    innerStrings.forEach((s, i) => {
+      const p = innerPoints[i * numInner % innerStrings.length]
+      pointStrings.push({ p, s })
+    })
+    outerStrings.forEach((s, i) => {
+      const p = outerPoints[i * numOuter % outerStrings.length]
+      pointStrings.push({ p, s })
+    })
+    pointStrings.forEach(({ s, p: { p: { x, y, z } } }) => {
+      const l = s.segmentLength
+      s.points.forEach((point, i) => {
+        point.x = x
+        point.y = y
+        point.z = z - i * l
+      })
+    })
+  }
+  setInitialPosition() {
+    const { innerDistance, innerRadius, size, theta1, theta2 } = this.shape
+    const innerPos = { d: innerDistance, r: innerRadius }
+    const middlePos = { d: size * Math.sin(theta1) / 2, r: size * Math.cos(theta1) / 2 }
+    const outerPos = { d: middlePos.d + size * Math.sin(theta2) / 2, r: middlePos.r + size * Math.cos(theta2) / 2 }
+    for (let i = 0; i < this.numInner; i++) {
+      const th = 2 * Math.PI * i / this.numInner
+      const cos = Math.cos(th)
+      const sin = Math.sin(th)
+      this.innerPoints.push(genPoint(innerPos.r * cos, innerPos.r * sin, -innerPos.d))
+    }
+    for (let i = 0; i < this.numOuter; i++) {
+      const th = 2 * Math.PI * i / this.numOuter
+      const cos = Math.cos(th)
+      const sin = Math.sin(th)
+      this.middlePoints.push(genPoint(middlePos.r * cos, middlePos.r * sin, -middlePos.d))
+      this.outerPoints.push(genPoint(outerPos.r * cos, outerPos.r * sin, -outerPos.d))
+    }
+    this.points.push(this.topPoint)
+    ;[this.innerPoints, this.middlePoints, this.outerPoints].forEach(points => {
+      points.forEach(p => this.points.push(p))
+    })
+  }
+  setLinks() {
+    const { numInner, numOuter, links, shrinkOutlineLinks, shrinkRadialLinks, topPoint, innerPoints, middlePoints, outerPoints, hardness } = this
+    for (let i = 0; i < numInner; i++) {
+      links.push(genLink(topPoint, innerPoints[i], hardness.topInner))
+      links.push(genLink(innerPoints[i], innerPoints[(i + 1) % numInner], hardness.inner))
+    }
+    for (let i = 0; i < numOuter; i++) {
+      links.push(genLink(topPoint, middlePoints[i], hardness.topMiddle))
+      links.push(genLink(middlePoints[i], middlePoints[(i + 1) % numOuter], hardness.middle))
+      shrinkOutlineLinks.push(genLink(outerPoints[i], outerPoints[(i + 1) % numOuter], hardness.outer))
+      links.push(genLink(middlePoints[i], outerPoints[i], hardness.middleOuter))
+      const inner = innerPoints[Math.round(i * numInner / numOuter) % numInner]
+      links.push(genLink(inner, middlePoints[i], hardness.innerMiddle))
+      shrinkRadialLinks.push(genLink(inner, outerPoints[i], hardness.innerMiddle))
+    }
+  }
+  resetForce() {
+    this.points.forEach(p => { p.f.x = p.f.y = p.f.z = 0 })
+  }
+  updateForce(links: JellyLink[], ratio: number) {
+    links.forEach(({ a, b, r, k }) => {
+      const dx = b.p.x - a.p.x
+      const dy = b.p.y - a.p.y
+      const dz = b.p.z - a.p.z
+      const distance = Math.hypot(dx, dy, dz)
+      const scale = (distance - r * ratio) * k / distance
+      const dvx = b.v.x - a.v.x
+      const dvy = b.v.y - a.v.y
+      const dvz = b.v.z - a.v.z
+      const vv = k / 2
+      a.f.x += scale * dx + dvx * vv
+      a.f.y += scale * dy + dvy * vv
+      a.f.z += scale * dz + dvz * vv
+      b.f.x -= scale * dx + dvx * vv
+      b.f.y -= scale * dy + dvy * vv
+      b.f.z -= scale * dz + dvz * vv
+    })
+  }
+  update(dt: number, muscle: number) {
+    this.updateForce(this.links, 1)
+    this.updateForce(this.shrinkRadialLinks, (2 + muscle) / 3)
+    this.updateForce(this.shrinkOutlineLinks, muscle)
+    this.points.forEach(({ p, v, f }) => {
+      v.x += f.x * dt
+      v.y += f.y * dt
+      v.z += f.z * dt
+      p.x += v.x * dt
+      p.y += v.y * dt
+      p.z += v.z * dt
+    })
+    this.pointStrings.forEach(({ p, s }) => {
+      s.update(p.f, dt)
+      const pos = s.points[0]
+      const v = s.velocities[0]
+      p.v.x = v.x
+      p.v.y = v.y
+      p.v.z = v.z
+      p.p.x = pos.x
+      p.p.y = pos.y
+      p.p.z = pos.z
+    })
+    this.resetForce()
+  }
+  renderToCanvas(ctx: CanvasRenderingContext2D) {
+    ctx.beginPath()
+    ;[...this.links, ...this.shrinkOutlineLinks, ...this.shrinkRadialLinks].forEach(({ a, b }) => {
+      ctx.moveTo(a.p.x, a.p.z)
+      ctx.lineTo(b.p.x, b.p.z)
+    })
+    ctx.stroke()
+  }
+}
