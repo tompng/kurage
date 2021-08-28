f0d80f0d8281d80e4efa1159925b872520f999e3
tompng <tomoyapenguin@gmail.com>
2021-08-28 21:41:55 +0900

    shrimp fish shape tweak

--- src/fish_shrimp.ts
+++ src/fish_shrimp.ts
@@ -5,30 +5,62 @@ type XYZ = [number, number, number]
 
 const shrimpOutline: [XY[], XY[]] = [
   [
-    [-0.9, 0.6],
-    [-0.7, 1],
-    [0.1, 1],
-    [0.7, 0.6],
+    [-1, 0.8],
+    [-0.5, 1],
+    [0.1, 0.9],
+    [0.6, 0.6],
+    [0.9, 0.2],
     [0.9, -0.2],
-    [0.7, -0.7],
-    [0.1, -0.8]
+    [0.6, -0.55],
+    [0.3, -0.65],
+    [0, -0.6]
   ],
   [
-    [-0.9, 0.6],
-    [-0.7, 0],
-    [-0.1, 0],
+    [-1, 0.8],
+    [-0.3, 0.2],
+    [-0.1, 0.1],
     [0.3, -0.1],
+    [0.35, -0.15],
     [0.4, -0.2],
-    [0.3, -0.5],
+    [0.3, -0.4],
+    [0.1, -0.45],
     [0, -0.6]
+  ]
+]
+const shrimpOther: [XYZ, XYZ, XYZ][] = [
+  [
+    [-0.3, -0.1, 0.2],
+    [-0.2, -0.1, 0.2],
+    [-0.6, -0.1, 0],
+  ],
+  [
+    [-0.1, -0.1, 0.15],
+    [0, -0.1, 0.15],
+    [-0.4, -0.1, -0.05],
+  ],
+  [
+    [0.1, -0.1, 0.1],
+    [0.2, -0.1, 0.1],
+    [-0.2, -0.1, -0.1],
+  ],
+  [
+    [-0.8, -0.05, 0.8],
+    [-1.4, -0.05, 1],
+    [-0.6, -0.05, 0.82],
+  ],
+  [
+    [-0.7, -0.05, 0.85],
+    [-1, -0.05, 1],
+    [-0.5, -0.05, 0.87],
   ]
 ]
 
 const fishOutline: [XY[], XY[]] = [
   [
     [-1, 0],
-    [-0.8, 0.3],
-    [-0.2, 0.5],
+    [-0.9, 0.2],
+    [-0.6, 0.4],
+    [-0.2, 0.45],
     [0.2, 0.4],
     [0.6, 0.2],
     [0.8, 0.4],
@@ -37,17 +69,18 @@ const fishOutline: [XY[], XY[]] = [
   ],
   [
     [-1, 0],
-    [-0.8, -0.3],
+    [-0.9, -0.2],
+    [-0.6, -0.35],
     [-0.2, -0.4],
-    [0.3, -0.4],
+    [0.3, -0.35],
     [0.6, -0.2],
     [0.8, -0.4],
     [0.95, -0.35],
-    [1, 0.2]
+    [1, -0.2]
   ]
 ]
 
-function createGeometry([up, down]: [XY[], XY[]], wfunc: (l: number, t: number) => number) {
+function createGeometry([up, down]: [XY[], XY[]], wfunc: (l: number, t: number) => number, other?: [XYZ, XYZ, XYZ][]) {
   const lines: XYZ[][] = [[], [], [], []]
   const xs = [...up.map(a => a[0]), ...down.map(a => a[0])]
   const ys = [...up.map(a => a[1]), ...down.map(a => a[1])]
@@ -67,11 +100,11 @@ function createGeometry([up, down]: [XY[], XY[]], wfunc: (l: number, t: number) 
   const geometry = new THREE.BufferGeometry()
   const positions: number[] = []
   const uvs: number[] = []
-  function add(a: XYZ, b: XYZ, c: XYZ) {
+  function add(a: XYZ, b: XYZ, c: XYZ, u?: number, v?: number) {
     positions.push(...a, ...b, ...c)
     positions.push(a[0], -a[1], a[2], c[0], -c[1], c[2], b[0], -b[1], b[2])
     ;[a, b, c, a, b, c].forEach(([x, _, z]) => {
-      uvs.push((x - xmin) / size, (z - ymin) / size)
+      uvs.push(u ?? (x - xmin) / size, v ?? (z - ymin) / size)
     })
   }
   for (let i = 0; i < up.length - 1; i++) {
@@ -83,6 +116,9 @@ function createGeometry([up, down]: [XY[], XY[]], wfunc: (l: number, t: number) 
       for (let k = 0; k < 3; k++) add(lines[k + 1][i], lines[k + 1][j], lines[k][j])
     }
   }
+  other?.forEach((tri) => {
+    add(...tri, 1, 1)
+  })
   const normals: number[] = []
   const ns = new Map<string, XYZ>()
   for (let i = 0; i < positions.length; i += 9) {
@@ -156,7 +192,7 @@ export function createFishGeometryMaterial() {
   ctx.fillStyle = '#888'
   ctx.fillRect(0, 0, 1, 1)
   ctx.beginPath()
-  ctx.arc(0.2, 0.8, 0.05, 0, 2 * Math.PI)
+  ctx.arc(0.2, 0.8, 0.03, 0, 2 * Math.PI)
   ctx.fillStyle = '#444'
   ctx.fill()
   ctx.fillStyle = '#444'
@@ -169,13 +205,14 @@ export function createFishGeometryMaterial() {
       vertexShader,
       fragmentShader,
       uniforms: { map: { value: map } },
-      blending: THREE.AdditiveBlending
+      blending: THREE.AdditiveBlending,
+      depthWrite: false
     })
   ] as const
 }
 
 export function createShrimpGeometryMaterial() {
-  const geometry = createGeometry(shrimpOutline, l => l / 3)
+  const geometry = createGeometry(shrimpOutline, l => l / 3, shrimpOther)
   const [canvas, ctx] = createPaint()
   ctx.fillStyle = '#822'
   ctx.fillRect(0, 0, 1, 1)
@@ -183,13 +220,15 @@ export function createShrimpGeometryMaterial() {
   ctx.arc(0.3, 0.3, 0.03, 0, 2 * Math.PI)
   ctx.fillStyle = '#222'
   ctx.fill()
-  ctx.fillStyle = '#622'
+  ctx.fillStyle = '#722'
   ctx.beginPath()
   ctx.moveTo(0.8, 0)
   ctx.lineTo(1, 0)
   ctx.lineTo(1, 1)
   ctx.lineTo(0, 1)
   ctx.fill()
+  ctx.fillStyle = '#411'
+  ctx.fillRect(0.9, 0, 0.1, 0.1)
   const map = new THREE.Texture(canvas)
   map.needsUpdate = true
   return [
@@ -198,7 +237,8 @@ export function createShrimpGeometryMaterial() {
       vertexShader,
       fragmentShader,
       uniforms: { map: { value: map } },
-      blending: THREE.AdditiveBlending
+      blending: THREE.AdditiveBlending,
+      depthWrite: false
     })
   ] as const
 }
