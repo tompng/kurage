bbdbd194d673293519fd405408b9eab1e737a9b5
tompng <tomoyapenguin@gmail.com>
2021-08-31 23:15:08 +0900

    close menu icon

--- src/icon.ts
+++ src/icon.ts
@@ -88,6 +88,20 @@ const icons = {
   book: bookPath,
   close: crossPath
 }
+
+function prepareCanvasContext(canvas: HTMLCanvasElement) {
+  const ratio = window.devicePixelRatio
+  const pixelSize = canvas.offsetWidth * ratio
+  canvas.width = canvas.height = pixelSize
+  const ctx = canvas.getContext('2d')
+  if (!ctx) return
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.save()
+  ctx.lineCap = ctx.lineJoin = 'round'
+  ctx.scale(pixelSize / 2, pixelSize / 2)
+  ctx.translate(1, 1)
+  return ctx
+}
 export class CanvasIcon {
   canvas: HTMLCanvasElement
   renderPath: (ctx: CanvasRenderingContext2D) => void
@@ -96,16 +110,8 @@ export class CanvasIcon {
     this.renderPath = typeof icon === 'function' ? icon : icons[icon]
   }
   render() {
-    const { canvas } = this
-    const ratio = window.devicePixelRatio
-    const pixelSize = this.canvas.offsetWidth * ratio
-    this.canvas.width = this.canvas.height = pixelSize
-    const ctx = canvas.getContext('2d')
+    const ctx = prepareCanvasContext(this.canvas)
     if (!ctx) return
-    ctx.save()
-    ctx.lineCap = ctx.lineJoin = 'round'
-    ctx.scale(pixelSize / 2, pixelSize / 2)
-    ctx.translate(1, 1)
     ctx.lineWidth = 1 / 8
     this.renderPath(ctx)
     ctx.strokeStyle = 'white'
@@ -114,28 +120,82 @@ export class CanvasIcon {
   }
 }
 
-export function test() {
-  const canvas = document.createElement('canvas')
-  canvas.width = canvas.height = 512
-  document.body.appendChild(canvas)
-  const ctx = canvas.getContext('2d')!
-  ctx.scale(40, 40)
-  ctx.translate(1, 1)
-  ctx.lineWidth = 0.1
-  ctx.lineCap = ctx.lineJoin = 'round'
-  ctx.strokeStyle = 'white'
-  ctx.beginPath()
-  tshirtPath(ctx)
-  ctx.stroke()
-  ctx.translate(2, 0)
-  ctx.beginPath()
-  bookPath(ctx)
-  ctx.stroke()
-  ctx.translate(2, 0)
-  ctx.beginPath()
-  mapPath(ctx)
-  ctx.stroke()
+export class CloseMenuIcon {
+  canvas = document.createElement('canvas')
+  phase = 1 // 0: jelly, 1: close
+  render() {
+    if (this.phase === 1) {
+      this.canvas.setAttribute('data-clickable', 'true')
+    } else {
+      this.canvas.removeAttribute('data-clickable')
+    }
+    const ctx = prepareCanvasContext(this.canvas)
+    if (!ctx) return
+    ctx.strokeStyle = 'white'
+    ctx.lineWidth = 0.08
+    const { phase } = this
+    const t = phase * phase * (3 - 2 * phase)
+    const jellyPoints: [number, number][] = []
+    for (let i = 0; i < 16; i++) {
+      const th = 2 * Math.PI * i / 16
+      const x = Math.cos(th)
+      let y = Math.sin(th) * (1 + 2 * t) / 3
+      if (y < 0) y -= Math.cos(6 * x) * (1 - x ** 8) ** 2 / 16 * (1 - t)
+      y -= (1 - t) * (x ** 2 + x ** 4) / 8
+      const rot = -Math.PI / 4 + t * Math.PI / 4
+      const x0 = (1 - t) * 0.4
+      const y0 = (1 - t) * 0.4
+      const size = 0.6 * t + (1 - t) * 0.7
+      const rcos = Math.cos(rot)
+      const rsin = Math.sin(rot)
+      const rndx = Math.sin(5 * th + 7 * t) - Math.sin(4 * th - 8 * t)
+      const rndy = Math.sin(4 * th + 6 * t) - Math.sin(5 * th - 7 * t)
+      const tx = x0 + size * (x * rcos - y * rsin)
+      const ty = y0 + size * (x * rsin + y * rcos)
+      const u = t * (1 - t) / 8
+      jellyPoints.push([tx * (1 - u) + u * rndx, ty * (1 - u) + u * rndy])
+    }
+    ctx.beginPath()
+    curvePath(ctx, jellyPoints, true)
+    if (t > 0.5) {
+      ctx.globalAlpha = t - 0.5
+      ctx.fillStyle = 'gray'
+      ctx.fill()
+      ctx.globalAlpha = 1
+    }
+    ctx.stroke()
+    ctx.beginPath()
+    function stringPath(ctx: CanvasRenderingContext2D, dir: number) {
+      const points: [number, number][] = []
+      for (let i = 0; i <= 5; i++) {
+        const s = i / 5
+        const s2 = 0.3 * (2 * s - 1)
+        const x2 = s2
+        const y2 = s2 * dir
+        const a = 0.2 + 0.2 * s * s + 0.1 * s
+        const x1 = 0.1 - s * 0.5 + dir * a - (1 - dir * dir) * (s + 0.2) / 8
+        const y1 = 0.1 - s * 0.5 - dir * a - (1 - dir * dir) * (s + 0.2) / 8
+        const u = t * (1 - t) / 4
+        const rndx = Math.sin((7 + dir / 2) * s + 7 * t + dir) - Math.sin(8 * s - 8 * t + dir)
+        const rndy = Math.cos(9 * s + 6 * t + dir) - Math.cos((7 + dir / 2) * s - 7 * t + dir)
+        points.push([x1 * (1 - t) + x2 * t + rndx * u, y1 * (1 - t) + y2 * t + rndy * u])
+      }
+      curvePath(ctx, points)
+    }
+    stringPath(ctx, -1)
+    stringPath(ctx, 1)
+    ctx.stroke()
+    const a = 1 - t
+    if (t < 0.5) {
+      ctx.globalAlpha = 1 - 2 * t
+      const points: [number, number][] = []
+      stringPath(ctx, 0)
+      ctx.stroke()
+    }
+    ctx.restore()
+  }
 }
+
 
 export const jellySvg = `
 <svg width=64 height=64 style='position:fixed;z-index:9999'>
